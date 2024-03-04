7717e691af4efcc1d56070be6566f73349a2d31b
kxag <kostasxagoraris@hotmail.com>
2024-03-04 08:53:30 +0200

    IsIntecompany

--- RadzenBlazorDemos/Models/Northwind/Customer.cs
+++ RadzenBlazorDemos/Models/Northwind/Customer.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Olympus.Cronus.DomainModel.Interfaces;
+
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RadzenBlazorDemos.Models.Northwind
